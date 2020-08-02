676475da2d6349848dc1c083359df162ccfb1b7f
IsabelaOliveira272018 <isabela.barros@compasso.com.br>
2020-08-02 19:21:55 -0300

    trabalhando com validação de listas

--- src/test/java/driverNew/Test.java
+++ src/test/java/driverNew/Test.java
@@ -1,5 +1,9 @@
 package driverNew;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -9,14 +13,34 @@ public class Test extends DriverNew{
 
 	
 	@org.junit.Test
-	public void realizaPesquisa() throws InterruptedException {
+	public void realizaPesquisa(){
 		WebElement search = driver.findElement(By.id("search"));
 		search.sendKeys("fortaleza digital", Keys.ENTER);
 		WebElement tituloLivro = driver.findElement(By.xpath("//h2/a"));
 		String livro = tituloLivro.getText();
 		Assert.assertEquals("[PRODUTO DE EXEMPLO] - Fortaleza Digital", livro);
+		WebElement elPreco = driver.findElement(By.cssSelector("#product-price-44 > span"));
+		String preco = elPreco.getText();
+		Assert.assertEquals("R$519,90", preco);
 		Thread.currentThread();
-		Thread.sleep(5000);
 	}
 	
+	@org.junit.Test
+	public void testClickList() {
+		//ul.products-grid > li
+		WebElement search = driver.findElement(By.id("search"));
+		search.sendKeys("html", Keys.ENTER);		
+		List<WebElement>  elLivros = driver.findElements(By.cssSelector("ul.products-grid > li"));
+		for (WebElement elLivro : elLivros) {
+			WebElement tituloLivro = elLivro.findElement(By.cssSelector("h2 > a"));
+			String elTituloLivro = tituloLivro.getText();
+			if (elTituloLivro.contains("Ajax com Java")) {
+				WebElement preco = elLivro.findElement(By.cssSelector("span.price"));
+			//	assertEquals("R$444,50", preco);
+				break;
+			}
+		}
+	
+	}
+
 }
