2933b631f99e12692cd41a25e4b4f7e39469042f
IsabelaOliveira272018 <isabela.barros@compasso.com.br>
2020-08-02 21:23:55 -0300

    atualizando framework

--- src/test/java/pages/FirstPage.java
+++ src/test/java/pages/FirstPage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
@@ -10,6 +12,7 @@ public class FirstPage {
 	String elSearch = "search";
 	String elTitleBook = "//h2/a";
 	String elPrice = "#product-price-44 > span";
+	String elBooksList = "ul.products-grid > li";
 	
 	//utilizamos o char sequence para que ele reconheça ações no browser, se fosse string ele não iria aceitar
 	public void setSearch(CharSequence...valor) {
@@ -29,6 +32,21 @@ public class FirstPage {
 		WebElement price = DriverNew.getDriver().findElement(By.cssSelector(elPrice));
 		return price.getText();
 	
+	}
+	
+	public String getPriceList() {
+		
+		
+		List<WebElement>  books = DriverNew.getDriver().findElements(By.cssSelector(elBooksList));
+		for (WebElement elLivro : elLivros) {
+			WebElement tituloLivro = elLivro.findElement(By.cssSelector("h2 > a"));
+			String elTituloLivro = tituloLivro.getText();
+			if (elTituloLivro.contains("Ajax com Java")) {
+				WebElement preco = elLivro.findElement(By.cssSelector("span.price"));
+			//	assertEquals("R$444,50", preco);
+				break;
+			}
+		}
 	}
 	
 	
